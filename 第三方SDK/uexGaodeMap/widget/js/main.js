9c0e1d1fc5ae5998cc5e0e8df40cf060d189380a
connyliu <21165491@qq.com>
2015-08-20 16:49:36 +0800

    上传高德地图case

---
+++ 第三方SDK/uexGaodeMap/widget/js/main.js
@@ -0,0 +1,14 @@
+function openNewWin(inWndName,html,inAniID,f,time)
+{
+	if (inAniID) {
+		uexWindow.open(inWndName,'0',html,inAniID,'','',(f)?f:4,time?time:250);
+	}
+	else {
+		uexWindow.open(inWndName, '0', html, '2', '', '', (f) ? f : 4);
+	}
+}
+
+function closeWin() {
+    uexGaodeMap.close();
+    uexWindow.close(-1);
+}
