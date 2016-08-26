a1c53d4dbb0ac3060b04b41bd2e1da3d5dcf2669
AppCanOpenSource <guangqiao.li@3g2win.com>
2016-08-26 11:51:12 +0800

    update document center script

--- DocumentCenter/run.swift
+++ DocumentCenter/run.swift
@@ -12,7 +12,7 @@ typealias MarkDownEditResult = (result: MarkDownSource,hasChanged: Bool)
 
 struct PluginMarkDown {
     struct Constant {
-        static let gitPath       = "/Users/appcan/Dev/Github/appcan-docs-v2"
+        static let gitPath       = "/Users/appcan_git/dev/appcan_git/appcan-docs-v2"
         static let pluginType    = ["系统功能","网络通讯","界面布局","功能扩展","第三方SDK"]
         static let ignorePlugins = []
     }
